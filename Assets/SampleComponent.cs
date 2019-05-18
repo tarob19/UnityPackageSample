69a22626d9d8f14c8dccd061d79d1f1836ebd4de
watanabe_toshimitsu <watanabe_toshimitsu@qualiarts.jp>
2019-05-19 00:32:13 +0900

    update module

---
+++ Assets/SampleComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleComponent : MonoBehaviour
+{
+	[SerializeField]
+    private UnityEngine.UI.Text _text;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
